0b97908f5ea0efc67763675231192105a1e9751e
mudit <mudit@mudit-ThinkPad-E420.(none)>
2013-08-20 09:29:10 +0530

    initial

---
+++ urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^$', 'jodibreaker.jodibreakerapp.views.index', name='index'),
+                       url(r'^home/', 'jodibreaker.jodibreakerapp.views.home', name='home'),
+                       url(r'^trendingjodis/','jodibreaker.jodibreakerapp.views.trendingjodi',name='trendingjodi'),
+                       url(r'^facebook/', include('django_facebook.urls')),
+                       url(r'^accounts/', include('django_facebook.auth_urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'jodibreaker.views.home', name='home'),
+    # url(r'^jodibreaker/', include('jodibreaker.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
+urlpatterns += patterns("django.views",
+        url(r'^media(?P<path>.*)/$',
+            "static.serve", {
+            "document_root": settings.MEDIA_ROOT,
+        })
+    )
