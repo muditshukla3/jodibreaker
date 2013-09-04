fa0152564a6f017c01b0acf4c76a7d23d1da0e99
root <root@ip-172-31-28-168.ap-southeast-1.compute.internal>
2013-09-04 17:23:55 +0000

    changes in form

--- jodibreakerapp/forms.py
+++ jodibreakerapp/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from models import Jodi
 
-JODI_CHOICE=list(Jodi.objects.values_list('id','jodi'))
-JODI_CHOICE.insert(0,(0,'-Choose Jodi-'))
- 
+
 class UserJodiForm(forms.Form):
     jodi=forms.ChoiceField(choices=[],widget=forms.Select(attrs={'class' : 'styled sel_jodi'}))
     jodi_custom=forms.CharField(widget=forms.TextInput(attrs={'class':'cus_jodi','maxlength':'50'}))
     
     def __init__(self):
         super(UserJodiForm,self).__init__()
+        JODI_CHOICE=list(Jodi.objects.values_list('id','jodi'))
+        JODI_CHOICE.insert(0,(0,'-Choose Jodi-'))
         self.fields['jodi'].choices=JODI_CHOICE
         
